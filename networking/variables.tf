671620ac76a522c1f8b54ded3e79cabfdd25c60d
CruzanCaramele <letters2hamza@gmnail.com>
2018-05-22 07:43:44 +0800

    set cidrs variable to list

--- networking/variables.tf
+++ networking/variables.tf
@@ -1,15 +1,12 @@
 variable "vpc_cidr" {
   description = "vpc cidr range"
-  default     = "10.123.0.0/16"
 }
 
 variable "public_cidrs" {
   type        = "list"
   description = "public cidrs"
-  default     = ["10.123.1.0/24", "10.123.2.0/24"]
 }
 
 variable "access_ip" {
   description = "allowed ip addresses"
-  default     = "0.0.0.0/0"
 }
