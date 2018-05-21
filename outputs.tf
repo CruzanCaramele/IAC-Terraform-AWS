1e707cd0cb2dee4f3a39e7489106d3e6ee40e1b2
CruzanCaramele <letters2hamza@gmnail.com>
2018-05-22 00:40:24 +0800

    bucket name resource

---
+++ outputs.tf
@@ -0,0 +1,6 @@
+#---------------------------------------------#
+# Storage outputs #
+#---------------------------------------------#
+output "Bucket Name" {
+  value = "${module.storage.bucketname}"
+}
