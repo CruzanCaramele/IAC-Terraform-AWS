54e0fed9417c2094cb58253fb6d8b53ad3396a99
CruzanCaramele <letters2hamza@gmnail.com>
2018-05-22 07:42:48 +0800

    define networking variables

--- variables.tf
+++ variables.tf
@@ -5,3 +5,16 @@ variable "aws_region" {
 variable "project_name" {
   description = "s3 bucket name uses the project name"
 }
+
+variable "vpc_cidr" {
+  description = "vpc cidr block range"
+}
+
+variable "public_cidrs" {
+  type        = "list"
+  description = "public cidr blocks"
+}
+
+variable "access_ip" {
+  description = "allowed ip addresses"
+}
