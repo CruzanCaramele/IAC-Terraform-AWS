ea92893d94d8d100570b18252115ad4cbb64e166
CruzanCaramele <letters2hamza@gmnail.com>
2018-05-22 07:42:35 +0800

    add networking module

--- main.tf
+++ main.tf
@@ -12,3 +12,13 @@ module "storage" {
   source       = "./storage"
   project_name = "${var.project_name}"
 }
+
+#---------------------------------------------#
+# Networking Reources #
+#---------------------------------------------#
+module "networking" {
+  source       = "./networking"
+  vpc_cidr     = "${var.vpc_cidr}"
+  public_cidrs = "${var.public_cidrs}"
+  access_ip    = "${var.access_ip}"
+}
