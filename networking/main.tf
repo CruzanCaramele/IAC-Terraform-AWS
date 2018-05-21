7577b7ea4401306541ebe4c1a790b13c880a3947
CruzanCaramele <letters2hamza@gmail.com>
2018-05-22 07:13:26 +0800

    add security group

--- networking/main.tf
+++ networking/main.tf
@@ -50,3 +50,56 @@ resource "aws_default_route_table" "prod_private_rt" {
     Name = "prod_private"
   }
 }
+
+#---------------------------------------------#
+# Subnets #
+#---------------------------------------------#
+resource "aws_subnet" "prod_public_subnet" {
+  count                  = 2
+  vpc_id                 = "${aws_vpc.prod_vpc.id}"
+  cidr_block             = "${var.public_cidrs[count.index]}"
+  map_public_ip_on_lunch = true
+  availability_zone      = "${data.aws_availability_zones.available_name[count.index]}"
+
+  tags {
+    Name = "prod_public_${count.index + 1}"
+  }
+}
+
+resource "aws_route_table_association" "prod_public_assoc" {
+  count          = "${aws_subnet.prod_public_subnet.count}"
+  subnet_id      = "${aws_subnet.prod_public_subnet.*.id[count.index]}"
+  route_table_id = "${aws_route_table.prod_public_rt.id}"
+}
+
+#---------------------------------------------#
+# Security group #
+#---------------------------------------------#
+resource "aws_security_group" "prod_public_sg" {
+  name        = "prod_public_sg"
+  description = "access to public instances"
+  vpc_id      = "${aws_vpc.prod_vpc.id}"
+
+  # SSH
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.access_ip}"]
+  }
+
+  # HTTP
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["${var.access_ip}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
