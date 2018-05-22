3661f1b029b427cd68d3cc790b2b6994c58f7367
CruzanCaramele <letters2hamza@gmnail.com>
2018-05-22 21:47:07 +0800

    add key name and public key variables

--- compute/variables.tf
+++ compute/variables.tf
@@ -1,1 +1,10 @@
+variable "key_name" {
+  description = "ssh key name"
+  default     = "prodkey"
+}
 
+variable "public_key_path" {
+  type        = "string"
+  description = "path to public key"
+  default     = "/Users/hamzayahaya/.ssh/id_rsa.pub"
+}
