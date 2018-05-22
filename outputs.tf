b6e57b9533b26e3960f66a47e4681c78fbb7c6bf
CruzanCaramele <letters2hamza@gmail.com>
2018-05-23 01:05:41 +0800

    out server details

--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,29 @@
 output "Bucket Name" {
   value = "${module.storage.bucketname}"
 }
+
+#---------------------------------------------#
+# Networking outputs #
+#---------------------------------------------#
+output "Public Subnets" {
+  value = "${join(", ", module.networking.public_subnets)}"
+}
+
+output "Subnet IPs" {
+  value = "${join(", ", module.networking.subnet_ips)}"
+}
+
+output "Public Security Group" {
+  value = "${module.networking.public_sg}"
+}
+
+#---------------------------------------------#
+# Compute outputs #
+#---------------------------------------------#
+output "Server IDs" {
+  value = "${module.compute.server_ids}"
+}
+
+output "Server IPs" {
+  value = "${module.compute.server_ips}"
+}
