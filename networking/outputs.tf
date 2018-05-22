eb09b83c7064a563702e7194ed91fed0ef24d871
CruzanCaramele <letters2hamza@gmnail.com>
2018-05-23 01:05:25 +0800

    output subnet and ips

---
+++ networking/outputs.tf
@@ -0,0 +1,11 @@
+output "public_subnets" {
+  value = "${aws_subnet.prod_public_subnet.*.id}"
+}
+
+output "public_sg" {
+  value = "${aws_security_group.prod_public_sg.id}"
+}
+
+output "subnet_ips" {
+  value = "${aws_subnet.prod_public_subnet.*.cidr_block}"
+}
