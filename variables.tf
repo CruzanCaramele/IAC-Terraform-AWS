2b3df04ed0f98a8f799cd1a7220b9ac1303dba3b
CruzanCaramele <letters2hamza@gmnail.com>
2018-05-22 22:49:51 +0800

    add varibels from compute module

--- variables.tf
+++ variables.tf
@@ -2,10 +2,16 @@ variable "aws_region" {
   description = "region to deploy resources"
 }
 
+#---------------------------------------------#
+# Storage Variables #
+#---------------------------------------------#
 variable "project_name" {
   description = "s3 bucket name uses the project name"
 }
 
+#---------------------------------------------#
+# Networking variables #
+#---------------------------------------------#
 variable "vpc_cidr" {
   description = "vpc cidr block range"
 }
@@ -17,4 +23,24 @@ variable "public_cidrs" {
 
 variable "access_ip" {
   description = "allowed ip addresses"
+}
+
+#---------------------------------------------#
+# Compute Variables #
+#---------------------------------------------#
+variable "key_name" {
+  description = "ssh key name"
+}
+
+variable "public_key_path" {
+  type        = "string"
+  description = "path to public key"
+}
+
+variable "instance_count" {
+  description = "number of instances"
+}
+
+variable "instance_type" {
+  description = "ec2 instance type to deploy"
 }
