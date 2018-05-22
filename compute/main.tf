80fb039652647a04d544ddb5dc0122e6b0d16fe6
CruzanCaramele <letters2hamza@gmnail.com>
2018-05-22 22:48:44 +0800

    add subnet and security groups

--- compute/main.tf
+++ compute/main.tf
@@ -18,7 +18,36 @@ data "aws_ami" "server_ami" {
 #---------------------------------------------#
 # Key Pair #
 #---------------------------------------------#
-resource "aws_key_pair" "tf_auth" {
+resource "aws_key_pair" "prod_auth" {
   key_name   = "${var.key_name}"
   public_key = "${file(var.public_key_path)}"
+}
+
+#---------------------------------------------#
+# Template File #
+#---------------------------------------------#
+data "template_file" "user-init" {
+  count    = 2
+  template = "${file("${path.module}/userdata.tpl")}"
+
+  vars {
+    firewall_subnets = "${element(var.subnet_ips, count.index)}"
+  }
+}
+
+#---------------------------------------------#
+# Instances #
+#---------------------------------------------#
+resource "aws_instance" "prod_server" {
+  count                  = "${var.instance_count}"
+  instance_type          = "${var.instance_type}"
+  ami                    = "${data.aws_ami.server_ami.id}"
+  key_name               = "${aws_key_pair.prod_auth.id}"
+  vpc_security_group_ids = ["${var.vpc_security_group}"]
+  subnet_id              = "${element(var.subnets, count.index)}"
+  user_data              = "${data.template_file.user-init.*.rendered[count.index]}"
+
+  tags {
+    Name = "prod_server-${count.index+1}"
+  }
 }
