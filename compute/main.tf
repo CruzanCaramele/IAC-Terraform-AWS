8792561d6c3a1e38d168223149330da28d9c55e5
CruzanCaramele <letters2hamza@gmnail.com>
2018-05-22 21:46:43 +0800

    create data source AMI, key pair and file function for key pair

--- compute/main.tf
+++ compute/main.tf
@@ -1,1 +1,24 @@
+#---------------------------------------------#
+# AMI Data Source #
+#---------------------------------------------#
+data "aws_ami" "server_ami" {
+  most_recent = true
 
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm*-x86_64-gp2"]
+  }
+}
+
+#---------------------------------------------#
+# Key Pair #
+#---------------------------------------------#
+resource "aws_key_pair" "tf_auth" {
+  key_name   = "${var.key_name}"
+  public_key = "${file(var.public_key_path)}"
+}
