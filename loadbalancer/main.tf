cf12f682e574016ade1a3217e016bf772d34a145
CruzanCaramele <letters2hamza@gmnail.com>
2018-05-24 22:08:05 +0800

    add access logs for lb to be stored in s3

--- loadbalancer/main.tf
+++ loadbalancer/main.tf
@@ -2,9 +2,20 @@
 # Load Balancer #
 #---------------------------------------------#
 resource "aws_lb" "prod-tf-alb" {
-  name               = "${var.aws_alb}"
-  internal           = "${var.load_balancer_is_internal}"
-  load_balancer_type = "application"
-  security_groups    = ["${var.lb_sg}"]
-  subnets            = ["${var.lb_subnets}"]
+  name                       = "${var.aws_alb}"
+  internal                   = "${var.load_balancer_is_internal}"
+  load_balancer_type         = "application"
+  security_groups            = ["${var.lb_sg}"]
+  subnets                    = ["${var.lb_subnets}"]
+  enable_deletion_protection = "${var.enable_deletion_protection}"
+
+  access_logs {
+    bucket  = "${var.lb_access_logs_bucket}"
+    prefix  = "${var.lb_access_logs_prefix}"
+    enabled = "${var.lb_access_logs_is_enabled}"
+  }
+
+  tags {
+    Environment = "${var.lb_environment}"
+  }
 }
