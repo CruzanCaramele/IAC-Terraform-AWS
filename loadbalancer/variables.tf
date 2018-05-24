e35773df5648725c329498ba939d98f2f7ab28f6
CruzanCaramele <letters2hamza@gmail.com>
2018-05-24 22:08:24 +0800

    add bucket, prefix and environment variables

--- loadbalancer/variables.tf
+++ loadbalancer/variables.tf
@@ -16,3 +16,23 @@ variable "lb_subnets" {
   type        = "list"
   description = "subnets for lb"
 }
+
+variable "enable_deletion_protection" {
+  description = "if enabled lb will be protected from deletion"
+}
+
+variable "lb_access_logs_bucket" {
+  description = "s3 bucket for stroring lb access logs"
+}
+
+variable "lb_access_logs_prefix" {
+  description = "folder to store logs within the s3 bucket"
+}
+
+variable "lb_access_logs_is_enabled" {
+  description = "enable logging - boolean value"
+}
+
+variable "lb_environment" {
+  description = "working environment"
+}
