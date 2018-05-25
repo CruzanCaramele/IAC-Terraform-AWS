a433acad2d380b6f71ac2d128f3baa012bb0b5e2
CruzanCaramele <letters2hamza@gmail.com>
2018-05-25 23:28:22 +0800

    add target group, target group attachment and listener for load balancer

--- loadbalancer/main.tf
+++ loadbalancer/main.tf
@@ -19,3 +19,27 @@ resource "aws_lb" "prod-tf-alb" {
     Environment = "${var.lb_environment}"
   }
 }
+
+resource "aws_lb_target_group" "prod-tf-tg" {
+  name     = "${var.lb_target_group_name}"
+  port     = "${var.lb_target_group_port}"
+  protocol = "HTTP"
+  vpc_id   = "${var.target_group_vpc_id}"
+}
+
+resource "aws_lb_target_group_attachment" "prod-tf-tg-attachment" {
+  target_group_arn = "${aws_lb_target_group.prod-tf-tg.arn}"
+  target_id        = "${var.num_instances}"
+  port             = "${var.lb_target_group_port}"
+}
+
+resource "aws_lb_listener" "prod-tf-listener" {
+  load_balancer_arn = "${aws_lb.prod-tf-alb.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = "${aws_lb_target_group.prod-tf-tg.arn}"
+    type             = "forward"
+  }
+}
