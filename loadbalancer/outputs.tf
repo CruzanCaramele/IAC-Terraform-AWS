175b5d907112cbdd821bf8362861268525a20e17
CruzanCaramele <letters2hamza@gmnail.com>
2018-05-27 00:58:01 +0800

    remove server id outputs for now

--- loadbalancer/outputs.tf
+++ loadbalancer/outputs.tf
@@ -1,3 +1,13 @@
 output "lb_name" {
   value = "${aws_lb.prod-tf-alb.dns_name}"
 }
+
+# output "targets" {
+#   value = "${aws_lb_target_group_attachment.target_id.id}"
+# }
+
+
+# output "server_ids" {
+#   value = "${module.compute.server_ids}"
+# }
+
