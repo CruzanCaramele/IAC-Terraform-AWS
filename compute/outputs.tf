0aa8001a97b2f983b23cc2abe7e1872924c119ea
CruzanCaramele <letters2hamza@gmail.com>
2018-05-27 18:53:16 +0800

    output server ids

--- compute/outputs.tf
+++ compute/outputs.tf
@@ -1,9 +1,13 @@
-output "server_id1" {
-  value = "${aws_instance.prod_server.0.id}"
-}
+# output "server_id1" {
+#   value = "${aws_instance.prod_server.0.id}"
+# }
 
-output "server_id2" {
-  value = "${aws_instance.prod_server.1.id}"
+# output "server_id2" {
+#   value = "${aws_instance.prod_server.1.id}"
+# }
+
+output "server_ids" {
+  value = "${aws_instance.prod_server.*.id}"
 }
 
 output "server_ips" {
