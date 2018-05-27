c80bd10b2d4d5bc71668bbf5acfb77e939e4150b
CruzanCaramele <letters2hamza@gmail.com>
2018-05-27 18:51:31 +0800

    add outputs for availabilty zones and security groups

--- networking/outputs.tf
+++ networking/outputs.tf
@@ -10,10 +10,22 @@ output "subnet_ips" {
   value = "${aws_subnet.prod_public_subnet.*.cidr_block}"
 }
 
+output "subnet_ids" {
+  value = "${aws_subnet.prod_public_subnet.*.id}"
+}
+
 output "prod_lb_sg" {
   value = "${aws_security_group.prod_lb_sg.id}"
 }
 
 output "vpc_id" {
   value = "${aws_vpc.prod_vpc.id}"
+}
+
+output "avail_zones" {
+  value = "${data.aws_availability_zones.available.names}"
+}
+
+output "security_group_ids" {
+  value = ["${aws_security_group.prod_public_sg.id}"]
 }
