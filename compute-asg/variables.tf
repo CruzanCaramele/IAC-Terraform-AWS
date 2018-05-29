f3fcd01376e272a0e28e5fe9f7e7243342a8a201
CruzanCaramele <letters2hamza@gmail.com>
2018-05-30 00:49:04 +0800

    launch config variables

--- compute-asg/variables.tf
+++ compute-asg/variables.tf
@@ -1,3 +1,14 @@
+#---------------------------------------------#
+# Launch Configuration Variables#
+#---------------------------------------------#
 variable "launch_config_name" {
   description = "name of launch configuration"
+}
+
+variable "launcg_config_instance_type" {
+  description = "the type of instance to launch"
+}
+
+variable "vpc_security_group" {
+  description = "security group of scaling group"
 }
